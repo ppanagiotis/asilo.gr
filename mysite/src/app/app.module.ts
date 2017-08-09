6e4b6f1297e8594cdbef427c57273fdfc42ef4d7
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-08-09 18:29:03 +0300

    Add head component

--- mysite/src/app/app.module.ts
+++ mysite/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +17,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeadComponent]
 })
 export class AppModule { }
